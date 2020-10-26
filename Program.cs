f2ef62a9b0de2b31ee3be5dc4fdb89cc8217d1c6
BlakeStanley5 <blakestanley542@gmail.com>
2020-10-26 10:45:14 -0500

    help

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} / {b} = {quotient} with a remainder of {remainder}");
+        }
+    }
+}
