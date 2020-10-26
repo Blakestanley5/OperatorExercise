a4745f89d3b61fc46c9bf041f11eb3cd5879f3e3
BlakeStanley5 <blakestanley542@gmail.com>
2020-10-26 11:45:15 -0500

    done

--- Program.cs
+++ Program.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿//using System;
 
-namespace OperatorExercise
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int a = 17;
-            int b = 4;
-            var quotient = a / b;
-            var remainder = a % b;
+//namespace OperatorExercise
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int a = 17;
+//            int b = 4;
+//            var quotient = a / b;
+//            var remainder = a % b;
 
-            Console.WriteLine($"{a} / {b} = {quotient} with a remainder of {remainder}");
-        }
-    }
-}
+//            Console.WriteLine($"{a} / {b} = {quotient} with a remainder of {remainder}");
+//            Console.WriteLine("what is the radius?");
+//            double redius = Convert.ToDouble(Console.ReadLine();
+
+//            var area = AreaOfCircle(r);
+
+//        }
+//        public static double AreaOfCircle(double r)
+//        {
+//            double pi = 3.14;
+           
+//            return pi - (r * r);
+            
+
+//        }
+
+//    }
+//}
